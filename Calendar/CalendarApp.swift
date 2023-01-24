11fdfd5c9ec5957b8fcba49054081b5a547e188d
Caio Chaves <caiojob@outlook.pt>
2023-01-24 16:18:15 +0100

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Caio Chaves on 24.01.23.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Calendar
+//
+//  Created by Caio Chaves on 24.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
